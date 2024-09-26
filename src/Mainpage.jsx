7b6e10c468dd3fd4d7545d779bc4bcb2fc818394
Kundai Jeoffrey Muchirawetu <kjmuchirawetu@gmail.com>
2024-09-26 16:26:37 +0200

    fixing welcome error

--- src/Mainpage.jsx
+++ src/Mainpage.jsx
@@ -6,7 +6,7 @@ import SearchFilters from "./search/SearchFilters";
 import Popover from "./Popover";
 import Footer from "./Footer";
 import Cardcontent from "./Cardcontent";
-import Welcome from "./welcome";
+import Welcome from "./Welcome";
 import { Route, Routes } from "react-router-dom";
 import Mainpage1 from "./Mainpage1";
 import Form from "./Form";
@@ -14,19 +14,17 @@ import Form from "./Form";
 function Mainpage() {
   return (
     <>
-
-    <Routes>
-      <Route path="/" element={<Welcome/>}/> 
-      <Route path="header" element={<Header />}/>
-      <Route path="search" element={<SearchFilters />}/>
-   <Route path="cardcontent" element={<Cardcontent/>}/>
-      <Route path="side" element={<Side />}/>
-      <Route path="footer" element={<Footer/>}/>
-     <Route path="main" element={ <Mainpage1/>}/>
-     <Route path="form" element={<Form/>}/>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Welcome />} />
+        <Route path="header" element={<Header />} />
+        <Route path="search" element={<SearchFilters />} />
+        <Route path="cardcontent" element={<Cardcontent />} />
+        <Route path="side" element={<Side />} />
+        <Route path="footer" element={<Footer />} />
+        <Route path="main" element={<Mainpage1 />} />
+        <Route path="form" element={<Form />} />
+      </Routes>
     </>
-    
   );
 }
 
